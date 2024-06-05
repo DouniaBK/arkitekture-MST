6f7c9d02592803e05a718ac53d3123626b12c8a5
DouniaBK <douniazedbacha@gmail.com>
2024-06-05 12:05:52 +0000

    add: basic style and set up

--- arkitechture/home/views.py
+++ arkitechture/home/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
-def home(request):
-    return HttpResponse("Hello, world!")
+def home_view(request):
+    return render(request, 'home/home.html')
+
+
+def about_view(request):
+    return render(request, 'home/about.html')
+
+
+def services_view(request):
+    return render(request, 'home/services.html')
+
+
+def contact_view(request):
+    return render(request, 'home/contact.html')
