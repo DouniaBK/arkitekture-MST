618a281ed453209a22ee7de0458283b4d37144ec
DouniaBK <douniazedbacha@gmail.com>
2024-08-03 10:35:04 +0000

    fix: settings for deployment

--- arkitechture/settings.py
+++ arkitechture/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e+di)yu))enu_w4nj)d#!0v11t2d*@^fhs&2m+7kmdbvp&_ms5'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
